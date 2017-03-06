30cc4f44a900002c6de1809dcaa3e1301e86878e
Neiza Nunez <neizapf@gmail.com>
2017-03-06 18:35:03 -0500

    mi url

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,14 +6,13 @@ function Print1(){
       window.addEventListener("click", function(event){
             event.preventDefault();
                   var texto1 = document.getElementById("records");
-                  var quiz1  = document.getElementById("quiz");
+                  var quiz1  = document.getElementById("recordsq");
                   var lista1 = "<ol>"+"<li>Funcdamentos de la programación</li>"+
                                       "<li>Funciones</li>"+
                                       "<li>Arreglos</li>"+
                                       "<li>Objetos</li>"+ "</ol>";
 
                   texto1.innerHTML="<br><b>Sprint 01: Introducción a Javascript</b> <br>" + lista1;
-
       })
 }
 
@@ -22,7 +21,7 @@ function Print2(){
       window.addEventListener("click", function(event){
             event.preventDefault();
                   var texto2 = document.getElementById("records");
-                  var quiz2  = document.getElementById("quiz");
+                  var quiz2  = document.getElementById("recordsq");
                   var lista2 = "<ol>"+"<li>Ciclo de vida del desarrollo web y arquitectura básica Ciente/Servidor / Confiiguración del abiente de trabajo e introdución al Git</li>"+
                                       "<li>HTML y Javascript en el nvegador</li>"+
                                       "<li>Manejo de comandos básicos de git en l terminal</li>"+
