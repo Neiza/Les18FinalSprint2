c205e4ce624c1a00df274cace690c1ec6bda2482
Neiza Nunez <neizapf@gmail.com>
2017-03-06 14:53:12 -0500

    tercer commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,11 @@
+
+
+window.addEventListerner(function(){
+  var button1 = document.getElementById("button1"),
+      button2 = document.getElementById("button2"),
+      button3 = document.getElementById("button3");
+
+  function(){
+    var 
+  }
+})
